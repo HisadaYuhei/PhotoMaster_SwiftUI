ab64de3b01229f4d5233efba14d195cbe1bfad5d
HisadaYuhei <hsdy20010918@gmail.com>
2025-06-23 01:55:32 +0900

    Initial Commit

---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by 久田　悠平 on 2025/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
